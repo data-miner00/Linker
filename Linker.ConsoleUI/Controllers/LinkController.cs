4f795a6d5d17486969f0a58828d94144ccf7f4cc
data-miner00 <data-miner00@users.noreply.github.com>
2022-03-23 01:23:01 +0800

    feat: Formatted DisplayAllLinks

--- Linker.ConsoleUI/Controllers/LinkController.cs
+++ Linker.ConsoleUI/Controllers/LinkController.cs
@@ -19,18 +19,38 @@ namespace Linker.ConsoleUI.Controllers
 
         public void DisplayAllLinks()
         {
-            Console.WriteLine("======================================");
-            Console.WriteLine("=============Display All==============");
-
             Console.WriteLine("List of collected links.");
+
             RenderLinks(this.linkRepository.GetAll());
 
-            void RenderLinks(IEnumerable<Link> links)
+            static void RenderLinks(IEnumerable<Link> links)
             {
-                Console.WriteLine("Index.\tName\t\tUrl\t\tDescription\t\t\tTags\t\t\tModified Date");
+                var displayTemplate = "|| {0} || {1} || {2} || {3} || {4} ||";
+                var indexPad = 6;
+                var namePad = 20;
+                var urlPad = 30;
+                var tagsPad = 20;
+                var modifiedPad = 23;
+
+                Console.WriteLine("=========================================================================================================================");
+                Console.WriteLine(
+                    displayTemplate,
+                    "Index".PadRight(indexPad),
+                    "Name".PadRight(namePad),
+                    "Url".PadRight(urlPad),
+                    "Tags".PadRight(tagsPad),
+                    "Modified Date".PadRight(modifiedPad));
+                Console.WriteLine("=========================================================================================================================");
+                
                 foreach (var (link, index) in links.WithIndex())
                 {
-                    Console.WriteLine("{0}.\t{1}\t\t{2}\t\t{3}\t\t\t{4}\t\t\t{5}", index, link.Name, link.Url, link.Description, string.Join(", ", link.Tags), link.ModifiedAt);
+                    Console.WriteLine(
+                        displayTemplate,
+                        (index + 1).ToString().PadRight(indexPad),
+                        link.Name.TruncateWithEllipsis(namePad - 3).PadRight(namePad),
+                        link.Url.TruncateWithEllipsis(urlPad - 3).PadRight(urlPad),
+                        string.Join(", ", link.Tags).TruncateWithEllipsis(tagsPad - 3).PadRight(tagsPad),
+                        link.ModifiedAt.ToString().PadRight(modifiedPad));
                 }
             }
         }
