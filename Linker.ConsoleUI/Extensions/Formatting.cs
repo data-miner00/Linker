61011df6e8dda7858d8b6450ea97c113b540deb4
data-miner00 <data-miner00@users.noreply.github.com>
2022-03-23 01:21:53 +0800

    feat: Added format extension

---
+++ Linker.ConsoleUI/Extensions/Formatting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linker.ConsoleUI.Extensions
+{
+    public static class Formatting
+    {
+        public static string TruncateWithEllipsis(this string input, int length = 30) =>
+            input.Length > length ? input.Substring(0, length) + "..." : input;
+    }
+}
