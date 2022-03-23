d5104defb54096a8f483fae6b5be1ecb361db5be
data-miner00 <data-miner00@users.noreply.github.com>
2022-03-23 23:41:54 +0800

    feat: Added null filter for empty string

---
+++ Linker.ConsoleUI/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linker.ConsoleUI.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string? ReturnNullIfEmpty(this string input) =>
+            string.IsNullOrEmpty(input) ? null : input;
+    }
+}
