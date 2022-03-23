4002b0405f277388c3f558a9814403a7d35ef77e
data-miner00 <data-miner00@users.noreply.github.com>
2022-03-23 21:51:03 +0800

    feat: Added pad side extension method

--- Linker.ConsoleUI/Extensions/Formatting.cs
+++ Linker.ConsoleUI/Extensions/Formatting.cs
@@ -8,5 +8,12 @@ namespace Linker.ConsoleUI.Extensions
     {
         public static string TruncateWithEllipsis(this string input, int length = 30) =>
             input.Length > length ? input.Substring(0, length) + "..." : input;
+
+        public static string PadSides(this string str, int totalWidth, char paddingChar = ' ')
+        {
+            int padding = totalWidth - str.Length;
+            int padLeft = padding / 2 + str.Length;
+            return str.PadLeft(padLeft, paddingChar).PadRight(totalWidth, paddingChar);
+        }
     }
 }
