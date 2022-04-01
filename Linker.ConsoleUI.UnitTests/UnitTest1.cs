5d02b0c6356ca2890c5a7cb1a13119182a9bf33a
data-miner00 <data-miner00@users.noreply.github.com>
2022-04-01 21:10:01 +0800

    chore: Provision empty test project for ConsoleUI

---
+++ Linker.ConsoleUI.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Linker.ConsoleUI.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
