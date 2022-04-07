e10c0228085ed25c54421be27feb6482de6a746f
data-miner00 <data-miner00@users.noreply.github.com>
2022-04-07 22:52:36 +0800

    feat: Added menu module

---
+++ Linker.ConsoleUI/UI/IMenu.cs
@@ -0,0 +1,17 @@
+﻿namespace Linker.ConsoleUI.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface IMenu
+    {
+        public void DisplayBanner();
+
+        public void PageHeader(string title, string version);
+
+        public void GenerateMenu(IEnumerable<string> items);
+
+        public void PageFooter(string copyright);
+    }
+}
---
+++ Linker.ConsoleUI/UI/Menu.cs
@@ -0,0 +1,51 @@
+﻿namespace Linker.ConsoleUI.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using Linker.ConsoleUI.Extensions;
+
+    public sealed class Menu : IMenu
+    {
+        public int Width { get; set; } = 61;
+
+        public void DisplayBanner()
+        {
+            Console.WriteLine();
+            Console.WriteLine("   █████        ███             █████                        ");
+            Console.WriteLine("  ░░███        ░░░             ░░███                         ");
+            Console.WriteLine("   ░███        ████  ████████   ░███ █████  ██████  ████████ ");
+            Console.WriteLine("   ░███       ░░███ ░░███░░███  ░███░░███  ███░░███░░███░░███");
+            Console.WriteLine("   ░███        ░███  ░███ ░███  ░██████░  ░███████  ░███ ░░░ ");
+            Console.WriteLine("   ░███      █ ░███  ░███ ░███  ░███░░███ ░███░░░   ░███     ");
+            Console.WriteLine("   ███████████ █████ ████ █████ ████ █████░░██████  █████    ");
+            Console.WriteLine("  ░░░░░░░░░░░ ░░░░░ ░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░░  ░░░░░     ");
+            Console.WriteLine();
+        }
+
+        public void PageHeader(string title, string version)
+        {
+            Console.WriteLine(string.Empty.PadLeft(this.Width, '='));
+            Console.WriteLine("|{0}|", string.Empty.PadLeft(this.Width - 2));
+            Console.WriteLine("|{0}|", title.PadSides(this.Width - 2));
+            Console.WriteLine("|{0}|", version.PadSides(this.Width - 2));
+            Console.WriteLine("|{0}|", string.Empty.PadLeft(this.Width - 2));
+            Console.WriteLine(string.Empty.PadLeft(this.Width, '='));
+        }
+
+        public void GenerateMenu(IEnumerable<string> items)
+        {
+            foreach (var (item, index) in items.WithIndex())
+            {
+                Console.WriteLine(string.Empty.PadLeft(this.Width, '='));
+                Console.WriteLine("|{0}|", $"{index + 1} - {item}".PadSides(this.Width - 2));
+            }
+
+            Console.WriteLine(string.Empty.PadLeft(this.Width, '='));
+        }
+
+        public void PageFooter(string copyright)
+        {
+            // wip
+        }
+    }
+}
