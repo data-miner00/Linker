a45f7bd32607ef6ddedb013492387027a21ffdbb
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-07 23:32:05 +0800

    feat: Scope the controller and repository for Link child only

--- Linker.Core/Controllers/IController.cs
+++ Linker.Core/Controllers/IController.cs
@@ -1,17 +1,42 @@
 ﻿namespace Linker.Core.Controllers
 {
+    using Linker.Core.Models;
+
+    /// <summary>
+    /// The base interface for link controllers.
+    /// </summary>
+    /// <typeparam name="T">The object that extends <see cref="Link"/>.</typeparam>
     public interface IController<T>
+        where T : Link
     {
+        /// <summary>
+        /// Navigates to display all items page.
+        /// </summary>
         public void DisplayAllItems();
 
+        /// <summary>
+        /// Navigates to display item details page.
+        /// </summary>
         public void DisplayItemDetails();
 
+        /// <summary>
+        /// Navigates to add new item page.
+        /// </summary>
         public void InsertItem();
 
+        /// <summary>
+        /// Navigates to update item page.
+        /// </summary>
         public void UpdateItem();
 
+        /// <summary>
+        /// Navigates to remove item page.
+        /// </summary>
         public void RemoveItem();
 
+        /// <summary>
+        /// Save the changes made.
+        /// </summary>
         public void SaveChanges();
     }
 }
--- Linker.Core/Repositories/IRepository.cs
+++ Linker.Core/Repositories/IRepository.cs
@@ -1,19 +1,50 @@
 ﻿namespace Linker.Core.Repositories
 {
     using System.Collections.Generic;
+    using Linker.Core.Models;
 
+    /// <summary>
+    /// The base interface for link repositories.
+    /// </summary>
+    /// <typeparam name="T">The object that extends <see cref="Link"/>.</typeparam>
     public interface IRepository<T>
+        where T : Link
     {
+        /// <summary>
+        /// Add a new item into the database.
+        /// </summary>
+        /// <param name="item">The item to be added.</param>
         public void Add(T item);
 
+        /// <summary>
+        /// Retrieves all the available data from the database.
+        /// </summary>
+        /// <returns>The entire list of data.</returns>
         public IEnumerable<T> GetAll();
 
+        /// <summary>
+        /// Retrieves the item by ID.
+        /// </summary>
+        /// <param name="id">The id of the item.</param>
+        /// <returns>The item itself.</returns>
         public T GetById(string id);
 
+        /// <summary>
+        /// Deletes an item from the database with the given id.
+        /// </summary>
+        /// <param name="id">The id of the item to be removed.</param>
         public void Remove(string id);
 
+        /// <summary>
+        /// Updates an item with the new details provided.
+        /// </summary>
+        /// <param name="item">The item with updated details.</param>
         public void Update(T item);
 
-        int Commit();
+        /// <summary>
+        /// Commit the changes to the database.
+        /// </summary>
+        /// <returns>0 for success and -1 for failed.</returns>
+        public int Commit();
     }
 }
