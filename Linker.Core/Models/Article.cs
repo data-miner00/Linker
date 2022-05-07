e98fe2ff5518b7f2b49cc2152c28da3d09185218
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-07 23:33:19 +0800

    refactor: Extract the links into link base class

--- Linker.Core/Models/Article.cs
+++ Linker.Core/Models/Article.cs
@@ -1,18 +1,10 @@
 ﻿namespace Linker.Core.Models
 {
-    using System;
-    using System.Collections.Generic;
-
     /// <summary>
-    /// The model for a website that is an article, blog or tutorial.
+    /// The model for a website that is an article.
     /// </summary>
-    public class Article
+    public class Article : Link
     {
-        /// <summary>
-        /// Gets or sets the unique identifier for the object.
-        /// </summary>
-        public string Id { get; set; }
-
         /// <summary>
         /// Gets or sets the title of the article.
         /// </summary>
@@ -29,16 +21,6 @@
         public int Year { get; set; }
 
         /// <summary>
-        /// Gets or sets the url of the article.
-        /// </summary>
-        public string Url { get; set; }
-
-        /// <summary>
-        /// Gets or sets the category of the article.
-        /// </summary>
-        public string Category { get; set; }
-
-        /// <summary>
         /// Gets or sets a value indicating whether the article is listed as watch later.
         /// </summary>
         public bool WatchLater { get; set; }
@@ -49,38 +31,8 @@
         public string Domain { get; set; }
 
         /// <summary>
-        /// Gets or sets the description/summary of the article.
-        /// </summary>
-        public string Description { get; set; }
-
-        /// <summary>
-        /// Gets or sets the tags of the article.
-        /// </summary>
-        public IEnumerable<string> Tags { get; set; }
-
-        /// <summary>
-        /// Gets or sets the language of the article.
-        /// </summary>
-        public Language Language { get; set; }
-
-        /// <summary>
         /// Gets or sets the grammatical level of the article.
         /// </summary>
         public Grammar Grammar { get; set; }
-
-        /// <summary>
-        /// Gets or sets the last visit of the article.
-        /// </summary>
-        public DateTime LastVisitAt { get; set; }
-
-        /// <summary>
-        /// Gets or sets the creation date of the article link entry.
-        /// </summary>
-        public DateTime CreatedAt { get; set; }
-
-        /// <summary>
-        /// Gets or sets the modified date of the article link entry.
-        /// </summary>
-        public DateTime ModifiedAt { get; set; }
     }
 }
---
+++ Linker.Core/Models/Link.cs
@@ -0,0 +1,56 @@
+﻿namespace Linker.Core.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The base model for all of the link related objects.
+    /// </summary>
+    public abstract class Link
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the object.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the url of the link.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the category of the link.
+        /// </summary>
+        public Category Category { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description/summary of the link.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tags of the link.
+        /// </summary>
+        public IEnumerable<string> Tags { get; set; }
+
+        /// <summary>
+        /// Gets or sets the language of the link.
+        /// </summary>
+        public Language Language { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last visit of the link.
+        /// </summary>
+        public DateTime LastVisitAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date of the link entry.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the modified date of the link entry.
+        /// </summary>
+        public DateTime ModifiedAt { get; set; }
+    }
+}
