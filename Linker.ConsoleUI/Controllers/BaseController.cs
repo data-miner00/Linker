627a4cd0e28e4f758cc1107f399e31e9f4e3e221
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-09 22:45:32 +0800

    docs: Document base controller class

--- Linker.ConsoleUI/Controllers/BaseController.cs
+++ Linker.ConsoleUI/Controllers/BaseController.cs
@@ -8,20 +8,38 @@
     using Linker.Core.Models;
     using Linker.Core.Repositories;
 
+    /// <summary>
+    /// The base class for <see cref="IController{T}"/> object.
+    /// Contains a few common functions.
+    /// </summary>
+    /// <typeparam name="T">The <see cref="Link"/> children.</typeparam>
     public abstract class BaseController<T> : IController<T>
         where T : Link
     {
         protected readonly IRepository<T> repository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseController{T}"/> class.
+        /// </summary>
+        /// <param name="repository">The <see cref="IRepository{T}"/> object.</param>
         protected BaseController(IRepository<T> repository)
         {
             this.repository = EnsureArg.IsNotNull(repository, nameof(repository));
         }
 
+        /// <summary>
+        /// A page to displays all items in the list.
+        /// </summary>
         public abstract void DisplayAllItems();
 
+        /// <summary>
+        /// A page to displays single item with it's details.
+        /// </summary>
         public abstract void DisplayItemDetails();
 
+        /// <summary>
+        /// A page to add a new item into the database.
+        /// </summary>
         public virtual void InsertItem()
         {
             Console.Clear();
@@ -37,6 +55,9 @@
             _ = this.PromptForInput("\nPress ENTER to return to main menu...", string.Empty);
         }
 
+        /// <summary>
+        /// A page to delete an item from the database.
+        /// </summary>
         public virtual void RemoveItem()
         {
             Console.Clear();
@@ -58,6 +79,9 @@
             _ = this.PromptForInput("\nPress ENTER to return to main menu...", string.Empty);
         }
 
+        /// <summary>
+        /// A page to update a single item in the list.
+        /// </summary>
         public virtual void UpdateItem()
         {
             Console.Clear();
@@ -84,11 +108,19 @@
             _ = this.PromptForInput("\nPress ENTER to return to main menu...", string.Empty);
         }
 
+        /// <summary>
+        /// Save all the changes made.
+        /// </summary>
         public virtual void SaveChanges()
         {
             this.repository.Commit();
         }
 
+        /// <summary>
+        /// A helper method to display all possible values of an enum.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="Enum"/> type.</typeparam>
+        /// <param name="name">The name of the Enum.</param>
         protected void DisplayEnum<T>(string name)
         {
             Console.WriteLine($"List of available {name}");
@@ -100,8 +132,17 @@
             }
         }
 
+        /// <summary>
+        /// Retrieves the input from the user customly.
+        /// </summary>
+        /// <returns>The built <see cref="Link"/> instance.</returns>
         protected abstract T GetItemFromInput();
 
+        /// <summary>
+        /// A helper method to display prompt and receive input as string.
+        /// </summary>
+        /// <param name="prompt">The text to be displayed.</param>
+        /// <returns>The input obtained.</returns>
         protected string PromptForInput(params object[] prompt)
         {
             var consoleWrite = typeof(Console)
