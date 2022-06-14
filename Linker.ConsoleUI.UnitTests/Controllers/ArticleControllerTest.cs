248374bf5c2cb306817e7fba07c86cbb3a30c5d5
data-miner00 <data-miner00@users.noreply.github.com>
2022-06-14 22:01:24 +0800

    test: Added simple article controller test

---
+++ Linker.ConsoleUI.UnitTests/Controllers/ArticleControllerTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Linker.ConsoleUI.UnitTests.Controllers
+{
+    using System;
+    using Linker.ConsoleUI.Controllers;
+    using Xunit;
+
+    public sealed class ArticleControllerTest
+    {
+        [Fact]
+        public void ArticleController_InvalidArgs_ShouldThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ArticleController(null));
+        }
+    }
+}
