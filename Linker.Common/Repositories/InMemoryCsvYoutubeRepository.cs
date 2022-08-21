1608d196fc08cc9fd4d0af3d8cf9ab8b8907ab71
Shaun Chong <mumk0313@gmail.com>
2022-08-21 20:29:06 +0800

    feat: Added Youtube repo and controller

---
+++ Linker.Common/Repositories/InMemoryCsvYoutubeRepository.cs
@@ -0,0 +1,131 @@
+﻿namespace Linker.Common.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Linker.Common.Helpers;
+    using Linker.Core.CsvModels;
+    using Linker.Core.Models;
+    using Linker.Core.Repositories;
+
+    public class InMemoryCsvYoutubeRepository : IInMemoryCsvRepository<Youtube, CsvYoutube>, IYoutubeRepository
+    {
+        private readonly string pathToData;
+
+        private List<Youtube> channels;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InMemoryCsvYoutubeRepository"/> class.
+        /// </summary>
+        public InMemoryCsvYoutubeRepository()
+        {
+            var filePath = ConfigurationResolver.GetConfig("YoutubeCsvPath");
+            this.pathToData = Path.Combine(Environment.CurrentDirectory, filePath);
+
+            this.channels = CsvHelper.Load<CsvYoutube, Youtube, YoutubeClassMap>(this.pathToData, this.CsvModelToModel);
+        }
+
+        /// <inheritdoc/>
+        public void Add(Youtube item)
+        {
+            var randomId = Guid.NewGuid().ToString();
+            item.Id = randomId;
+            this.channels.Add(item);
+        }
+
+        /// <inheritdoc/>
+        public int Commit()
+        {
+            return CsvHelper.Save(this.pathToData, this.ModelToCsvModel, this.channels);
+        }
+
+        /// <inheritdoc/>
+        public Youtube CsvModelToModel(CsvYoutube csvModel)
+        {
+            return new Youtube
+            {
+                Id = csvModel.Id,
+                Url = csvModel.Url,
+                Category = (Category)csvModel.Category,
+                Description = csvModel.Description,
+                Tags = csvModel.Tags.Split('|'),
+                Language = (Language)csvModel.Language,
+                LastVisitAt = csvModel.LastVisitAt,
+                CreatedAt = csvModel.CreatedAt,
+                ModifiedAt = csvModel.ModifiedAt,
+                Name = csvModel.Name,
+                Youtuber = csvModel.Youtuber,
+                Country = csvModel.Country,
+            };
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Youtube> GetAll()
+        {
+            return from channel in this.channels
+                   orderby channel.CreatedAt
+                   select channel;
+        }
+
+        /// <inheritdoc/>
+        public Youtube GetById(string id)
+        {
+            var link = from channel in this.channels
+                       where channel.Id == id
+                       select channel;
+
+            return link.FirstOrDefault();
+        }
+
+        /// <inheritdoc/>
+        public CsvYoutube ModelToCsvModel(Youtube model)
+        {
+            return new CsvYoutube
+            {
+                Id = model.Id,
+                Url = model.Url,
+                Category = (int)model.Category,
+                Description = model.Description,
+                Tags = string.Join('|', model.Tags),
+                Language = (int)model.Language,
+                LastVisitAt = model.LastVisitAt,
+                CreatedAt = model.CreatedAt,
+                ModifiedAt = model.ModifiedAt,
+                Name = model.Name,
+                Youtuber = model.Youtuber,
+                Country = model.Country,
+            };
+        }
+
+        /// <inheritdoc/>
+        public void Remove(string id)
+        {
+            this.channels = (from channel in this.channels
+                             where channel.Id != id
+                             select channel).ToList();
+        }
+
+        /// <inheritdoc/>
+        public void Update(Youtube item)
+        {
+            var link = this.channels.FirstOrDefault(channel => channel.Id == item.Id);
+
+            if (link == null)
+            {
+                throw new InvalidOperationException("Cannot find the link with id");
+            }
+
+            link.Url = item.Url ?? link.Url;
+            link.Description = item.Description ?? link.Description;
+            link.Category = item.Category;
+            link.Tags = item.Tags ?? link.Tags;
+            link.Language = item.Language;
+            link.Youtuber = item.Youtuber ?? link.Youtuber;
+            link.Country = item.Country ?? link.Country;
+            link.Name = item.Name ?? link.Name;
+
+            link.ModifiedAt = DateTime.Now;
+        }
+    }
+}
---
+++ Linker.ConsoleUI/Controllers/YoutubeController.cs
@@ -0,0 +1,201 @@
+﻿namespace Linker.ConsoleUI.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Linker.Common.Extensions;
+    using Linker.ConsoleUI.Extensions;
+    using Linker.Core.Controllers;
+    using Linker.Core.Models;
+    using Linker.Core.Repositories;
+
+    internal class YoutubeController : BaseController<Youtube>, IYoutubeController
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="YoutubeController"/> class.
+        /// </summary>
+        /// <param name="youtubeRepository">The <see cref="IYoutubeRepository"/> object.</param>
+        public YoutubeController(IYoutubeRepository youtubeRepository)
+            : base(youtubeRepository)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void DisplayAllItems()
+        {
+            Console.Clear();
+            Console.WriteLine("List of collected links.");
+
+            RenderLinks(this.repository.GetAll());
+
+            _ = this.PromptForInput("Press ENTER to return to main menu...", string.Empty);
+
+            static void RenderLinks(IEnumerable<Youtube> links)
+            {
+                const string displayTemplate = "|| {0} || {1} || {2} || {3} || {4} || {5} ||";
+                const int dividerLength = 163;
+                const int indexPad = 6;
+                const int idPad = 38;
+                const int namePad = 20;
+                const int urlPad = 30;
+                const int tagsPad = 20;
+                const int modifiedPad = 23;
+
+                Console.WriteLine(string.Empty.PadRight(dividerLength, '='));
+                Console.WriteLine(
+                    displayTemplate,
+                    "Index".PadRight(indexPad),
+                    "Id".PadRight(idPad),
+                    "Name".PadRight(namePad),
+                    "Url".PadRight(urlPad),
+                    "Tags".PadRight(tagsPad),
+                    "Modified Date".PadRight(modifiedPad));
+                Console.WriteLine(string.Empty.PadRight(dividerLength, '='));
+
+                foreach (var (link, index) in links.WithIndex())
+                {
+                    Console.WriteLine(
+                        displayTemplate,
+                        (index + 1).ToString().PadRight(indexPad),
+                        link.Id.PadRight(idPad),
+                        link.Name.TruncateWithEllipsis(namePad - 3).PadRight(namePad),
+                        link.Url.TruncateWithEllipsis(urlPad - 3).PadRight(urlPad),
+                        string.Join(", ", link.Tags).TruncateWithEllipsis(tagsPad - 3).PadRight(tagsPad),
+                        link.ModifiedAt.ToString().PadRight(modifiedPad));
+                }
+
+                Console.WriteLine(string.Empty.PadRight(dividerLength, '='));
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void DisplayItemDetails()
+        {
+            Console.Clear();
+            var id = this.PromptForInput("Enter the ID of the link: ", string.Empty);
+
+            try
+            {
+                var link = this.repository.GetById(id);
+
+                const int dividerLength = 100;
+                const int labelPad = 15;
+                const int contentPad = 83;
+
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '='));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, ' '));
+                Console.WriteLine("|{0}|", link.Name.PadSides(dividerLength));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, ' '));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '='));
+                Console.WriteLine("|{0}: {1}|", "Id".PadRight(labelPad), link.Id.PadRight(contentPad));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '-'));
+                Console.WriteLine("|{0}: {1}|", "Url".PadRight(labelPad), link.Url.PadRight(contentPad));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '-'));
+                Console.WriteLine("|{0}: {1}| {2}: {3}|", "Language".PadRight(labelPad), link.Language.ToString().PadRight(30), "Youtuber".PadRight(labelPad), link.Youtuber.PadRight(34));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '-'));
+                Console.WriteLine("|{0}: {1}|", "Description".PadRight(labelPad), link.Description.TruncateWithEllipsis(contentPad - 3).PadRight(contentPad));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '-'));
+                Console.WriteLine("|{0}: {1}|", "Tags".PadRight(labelPad), string.Join(", ", link.Tags).PadRight(contentPad));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '-'));
+                Console.WriteLine("|{0}: {1}| {2}: {3}|", "Created".PadRight(labelPad), link.CreatedAt.ToString().PadRight(30), "Modified".PadRight(labelPad), link.ModifiedAt.ToString().PadRight(34));
+                Console.WriteLine("|{0}|", string.Empty.PadRight(dividerLength, '='));
+
+                Console.WriteLine("\nActions");
+                Console.WriteLine("1. Visit link");
+                Console.WriteLine("2. Get full URL");
+                Console.WriteLine("3. Get full description");
+                Console.WriteLine("4. Return\n");
+
+                while (true)
+                {
+                    var input = this.PromptForInput("> ", string.Empty);
+                    switch (input)
+                    {
+                        case "1":
+                            System.Diagnostics.Process.Start("chrome.exe", link.Url);
+                            break;
+                        case "2":
+                            Console.WriteLine("Full link: {0}", link.Url);
+                            break;
+                        case "3":
+                            Console.WriteLine("Full description: {0}", link.Description);
+                            break;
+                        case "4":
+                            return;
+                        default:
+                            Console.WriteLine("Please insert the correct selection!");
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override Youtube GetItemFromInput()
+        {
+            uint category, language;
+
+            const string labelTemplate = "{0}: ";
+            const int labelPad = 13;
+
+            var name = this.PromptForInput(labelTemplate, "Name".PadRight(labelPad));
+            var url = this.PromptForInput(labelTemplate, "Url".PadRight(labelPad));
+            var youtuber = this.PromptForInput(labelTemplate, "Youtuber".PadRight(labelPad));
+            var description = this.PromptForInput(labelTemplate, "Description".PadRight(labelPad));
+            var country = this.PromptForInput(labelTemplate, "Country".PadRight(labelPad));
+
+            Console.WriteLine();
+
+            this.DisplayEnum<Category>(nameof(Category));
+            while (true)
+            {
+                try
+                {
+                    category = Convert.ToUInt32(this.PromptForInput(labelTemplate, "Category".PadRight(labelPad)));
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: {0}", ex.Message);
+                }
+            }
+
+            Console.WriteLine();
+
+            this.DisplayEnum<Language>(nameof(Language));
+            while (true)
+            {
+                try
+                {
+                    language = Convert.ToUInt32(this.PromptForInput(labelTemplate, "Language".PadRight(labelPad)));
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: {0}", ex.Message);
+                }
+            }
+
+            Console.WriteLine();
+
+            var _tags = this.PromptForInput(labelTemplate, "Tags".PadRight(labelPad));
+            var tags = _tags.Split(",").Select(tag => tag.Trim());
+
+            return new Youtube
+            {
+                Name = name,
+                Url = url,
+                Youtuber = youtuber,
+                Country = country,
+                Category = (Category)category,
+                Description = description,
+                Tags = tags,
+                Language = (Language)language,
+            };
+        }
+    }
+}
