7ed250885eb8a9e0c949fd054e033d8f38b9e15b
data-miner00 <data-miner00@users.noreply.github.com>
2022-09-12 22:15:46 +0800

    chore: Supress documentation warning for test projects

---
+++ Linker.Common.UnitTests/GlobalSupression.cs
@@ -0,0 +1,3 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Documentation Rules", "SA1600", Justification = "Test project does not need to be documented")]
---
+++ Linker.ConsoleUI.UnitTests/GlobalSupression.cs
@@ -0,0 +1,3 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Documentation Rules", "SA1600", Justification = "Test project does not need to be documented")]
