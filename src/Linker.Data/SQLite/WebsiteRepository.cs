3ef69f42147f127c90289061ada4367578d92599
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-29 23:26:28 +0800

    wip: Website Repository

--- src/Linker.Data/SQLite/WebsiteRepository.cs
+++ src/Linker.Data/SQLite/WebsiteRepository.cs
@@ -1,7 +1,9 @@
 ﻿namespace Linker.Data.SQLite
 {
     using System.Data;
+    using System.Security.Policy;
     using Dapper;
+    using EnsureThat;
     using Linker.Core.Models;
     using Linker.Core.Repositories;
 
@@ -11,24 +13,21 @@
 
         public WebsiteRepository(IDbConnection connection)
         {
-            this.connection = connection;
+            this.connection = EnsureArg.IsNotNull(connection);
         }
 
         /// <inheritdoc/>
         public void Add(Website item)
         {
-            var query = @"
-                INSERT INTO Websites (
+            var randomId = Guid.NewGuid().ToString();
+
+            var operation = @"
+                INSERT INTO Links (
                     Id,
                     Url,
                     Category,
                     Description,
                     Language,
-                    Name,
-                    Domain,
-                    Aesthetics,
-                    IsSubdomain,
-                    IsMultilingual,
                     LastVisitAt,
                     CreatedAt,
                     ModifiedAt
@@ -38,18 +37,90 @@
                     @Category,
                     @Description,
                     @Language,
-                    @Name,
-                    @Domain,
-                    @Aesthetics,
-                    @IsSubdomain,
-                    @IsMultilingual,
                     @LastVisitAt,
                     @CreatedAt,
                     @ModifiedAt
                 );
             ";
 
-            this.connection.Execute(query, item);
+            var operation2 = @"
+                INSERT INTO Websites (
+                    LinkId,
+                    Name,
+                    Domain
+                    Aesthetics
+                    IsSubdomain
+                    IsMultilingual
+                ) VALUES (
+                    @LinkId,
+                    @Name,
+                    @Domain,
+                    @Aesthetics,
+                    @IsSubdomain,
+                    @IsMultilingual
+                );
+            ";
+
+            var query = @"SELECT (Id) FROM Tags WHERE Name = @Name;";
+
+            var operation3 = @"
+                INSERT INTO Tags (
+                    Id,
+                    Name
+                ) VALUES (
+                    @Id,
+                    @Name
+                );
+            ";
+
+            var operation4 = @"
+                INSERT INTO Links_Tags (
+                    LinkId,
+                    TagId
+                ) VALUES (
+                    @LinkId,
+                    @TagId
+                );
+            ";
+
+            this.connection.Execute(operation, new
+            {
+                Id = randomId,
+                Url = item.Url,
+                Category = item.Category.ToString(),
+                Description = item.Description,
+                Language = item.Language.ToString(),
+                LastVisitAt = item.LastVisitAt,
+                CreatedAt = item.CreatedAt,
+                ModifiedAt = item.ModifiedAt,
+            });
+
+            this.connection.Execute(operation2, new
+            {
+                LinkId = randomId,
+                item.Name,
+                item.Domain,
+                Aesthetics = item.Aesthetics.ToString(),
+                item.IsSubdomain,
+                item.IsMultilingual,
+            });
+
+            foreach (var tag in item.Tags)
+            {
+                var result = this.connection.Query(query, new { Name = tag });
+                var randomId2 = Guid.NewGuid().ToString();
+
+                if (!result.Any())
+                {
+                    this.connection.Execute(operation3, new
+                    {
+                        Id = randomId2,
+                        Name = tag,
+                    });
+                }
+
+                this.connection.Execute(operation4, new { LinkId = randomId, TagId = randomId2 });
+            }
         }
 
         /// <inheritdoc/>
@@ -77,13 +148,13 @@
         public void Remove(string id)
         {
             var query = @"DELETE FROM Website WHERE Id = @Id;";
-            this.connection.Execute(query, new { Id = id, });
+            this.connection.Execute(query, new { Id = id });
         }
 
         /// <inheritdoc/>
         public void Update(Website item)
         {
-            var query = @"UPDATE Website SET Categpry = @Category WHERE Id = @Id;";
+            var query = @"UPDATE Website SET Category = @Category WHERE Id = @Id;";
 
             this.Remove(item.Id);
             this.Add(item);
