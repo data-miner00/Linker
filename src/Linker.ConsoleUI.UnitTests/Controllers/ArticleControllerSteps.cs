72aee9c890d6be82b390dedcf78915e92df72091
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-07 16:50:47 +0800

    test: Added test for ArticleController

---
+++ src/Linker.ConsoleUI.UnitTests/Controllers/ArticleControllerSteps.cs
@@ -0,0 +1,75 @@
+﻿namespace Linker.ConsoleUI.UnitTests.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Linker.ConsoleUI.Controllers;
+    using Linker.Core.Controllers;
+    using Linker.Core.Models;
+    using Linker.Core.Repositories;
+    using Linker.TestCore;
+    using Moq;
+
+    internal class ArticleControllerSteps : BaseSteps<ArticleControllerSteps>
+    {
+        private readonly Mock<ICsvArticleRepository> mockArticleRepository;
+        private readonly IArticleController articleController;
+
+        public ArticleControllerSteps()
+        {
+            this.mockArticleRepository = new Mock<ICsvArticleRepository>();
+            this.articleController = new ArticleController(this.mockArticleRepository.Object);
+        }
+
+        public ArticleControllerSteps GivenIHaveReadLineInput(string input)
+        {
+            Console.SetIn(new StringReader(input));
+            return this;
+        }
+
+        public ArticleControllerSteps GivenIMockConsoleOutput()
+        {
+            Console.SetOut(new StringWriter());
+            return this;
+        }
+
+        public ArticleControllerSteps GivenIHaveArticles(IEnumerable<Article> articles)
+        {
+            this.mockArticleRepository.Setup(x => x.GetAll()).Returns(articles);
+            return this;
+        }
+
+        public ArticleControllerSteps ThenIExpectRepositoryGetAllToBeCalled(Times times)
+        {
+            this.mockArticleRepository.Verify(x => x.GetAll(), times);
+
+            return this;
+        }
+
+        public ArticleControllerSteps GivenIHaveArticleDetails(Article article)
+        {
+            this.mockArticleRepository.Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns(article);
+            return this;
+        }
+
+        public ArticleControllerSteps ThenIExpectRepositoryGetByIdToBeCalled(Times times)
+        {
+            this.mockArticleRepository.Verify(x => x.GetById(It.IsAny<string>()), times);
+
+            return this;
+        }
+
+        public ArticleControllerSteps WhenIDisplayAllItems()
+        {
+            return this.RecordException(() => this.articleController.DisplayAllItems());
+        }
+
+        public ArticleControllerSteps WhenIDisplayItemDetails()
+        {
+            return this.RecordException(() => this.articleController.DisplayItemDetails());
+        }
+
+        public override ArticleControllerSteps GetSteps() => this;
+    }
+}
--- src/Linker.ConsoleUI.UnitTests/Controllers/ArticleControllerTest.cs
+++ src/Linker.ConsoleUI.UnitTests/Controllers/ArticleControllerTest.cs
@@ -1,15 +1,63 @@
 ﻿namespace Linker.ConsoleUI.UnitTests.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using Linker.ConsoleUI.Controllers;
+    using Linker.Core.Models;
+    using Moq;
     using Xunit;
 
     public sealed class ArticleControllerTest
     {
+        private readonly ArticleControllerSteps steps = new ArticleControllerSteps();
+
         [Fact]
         public void ArticleController_InvalidArgs_ShouldThrow()
         {
             Assert.Throws<ArgumentNullException>(() => new ArticleController(null));
+        }
+
+        [Fact]
+        public void DisplayAllItems_RepositoryGetAllCalled()
+        {
+            var articles = new List<Article>
+            {
+                new Article
+                {
+                    Id = "123",
+                    Url = "https://www.google.com/blog",
+                    Category = Category.Education,
+                    Description = "A long description",
+                    Tags = new List<string> { "Tag" },
+                    Language = Language.English,
+                    LastVisitAt = DateTime.Now,
+                    CreatedAt = DateTime.Now,
+                    ModifiedAt = DateTime.Now,
+                    Title = "mock title",
+                    Author = "mock author",
+                    Year = 1999,
+                    WatchLater = false,
+                    Domain = "www.google.com",
+                    Grammar = Grammar.Unknown,
+                },
+            };
+
+            this.steps
+                .GivenIHaveReadLineInput("Input")
+                .GivenIHaveArticles(articles)
+                .WhenIDisplayAllItems()
+                .ThenIExpectNoExceptionIsThrown()
+                .ThenIExpectRepositoryGetAllToBeCalled(Times.Once());
+        }
+
+        [Fact]
+        public void DisplayItemDetails_RepositoryGetByIdCalled()
+        {
+            this.steps
+                .GivenIHaveReadLineInput("Input")
+                .WhenIDisplayItemDetails()
+                .ThenIExpectNoExceptionIsThrown()
+                .ThenIExpectRepositoryGetByIdToBeCalled(Times.Once());
         }
     }
 }
