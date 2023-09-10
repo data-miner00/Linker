ef3a410a190895eabf21f8f542bf9030dfabdef0
data-miner00 <data-miner00@users.noreply.github.com>
2023-09-10 11:18:06 +0800

    style: Use top level statement

--- src/Linker.WebApi/Program.cs
+++ src/Linker.WebApi/Program.cs
@@ -1,42 +1,38 @@
-namespace Linker.WebApi
+using System.Data.SQLite;
+using Linker.Core.Repositories;
+using Linker.Data.SQLite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var sqliteConnectionString = builder.Configuration["SQLite:ConnectionString"];
+
+using var connection = new SQLiteConnection(sqliteConnectionString);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IWebsiteRepository>(
+    x => new WebsiteRepository(connection));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    using System.Data.SQLite;
-    using Linker.Core.Repositories;
-    using Linker.Data.SQLite;
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var sqliteConnectionString = builder.Configuration["SQLite:ConnectionString"];
-            using var connection = new SQLiteConnection(sqliteConnectionString);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<IWebsiteRepository>(
-                x => new WebsiteRepository(connection));
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app
-                    .UseSwagger()
-                    .UseSwaggerUI();
-            }
-
-            app
-                .UseExceptionHandler("/error")
-                .UseHttpsRedirection()
-                .UseAuthorization();
-
-            app.MapControllers();
-            app.Run();
-        }
-    }
+    app
+        .UseSwagger()
+        .UseSwaggerUI();
 }
+
+app
+    .UseExceptionHandler("/error")
+    .UseHttpsRedirection()
+    .UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
