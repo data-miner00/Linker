4094a7212eb3c5c91557af57de39cefa38a0227e
data-miner00 <data-miner00@users.noreply.github.com>
2023-09-28 06:56:41 +0800

    fix: Explicit http binding for Swagger

--- src/Linker.WebApi/Controllers/ErrorController.cs
+++ src/Linker.WebApi/Controllers/ErrorController.cs
@@ -2,9 +2,17 @@
 {
     using Microsoft.AspNetCore.Mvc;
 
+    /// <summary>
+    /// The global exception handler.
+    /// </summary>
     [Route("/error")]
     public class ErrorController : ControllerBase
     {
+        /// <summary>
+        /// The handler for exceptions.
+        /// </summary>
+        /// <returns>Wrapped Http response.</returns>
+        [HttpGet("", Name = "Error")]
         public IActionResult Index()
         {
             return this.Problem();
