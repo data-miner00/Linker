5da1d211da6b66214355adb5b58fc565b5b5e2e7
data-miner00 <data-miner00@users.noreply.github.com>
2024-01-17 21:12:03 +0800

    feat: Update roles

--- src/Linker.Core/Models/Role.cs
+++ src/Linker.Core/Models/Role.cs
@@ -6,9 +6,21 @@
     public enum Role
     {
         /// <summary>
+        /// The owner of the application. Have full access to the system
+        /// including managing administrators.
+        /// </summary>
+        Owner,
+
+        /// <summary>
         /// The administrator that has full access to the system.
         /// </summary>
         Administrator,
+
+        /// <summary>
+        /// A delegated user that has access to edit or delete content,
+        /// but not creation.
+        /// </summary>
+        Moderator,
 
         /// <summary>
         /// A regular user that uses the application.
@@ -16,7 +28,13 @@
         User,
 
         /// <summary>
-        /// A guest user that only have read access to the application.
+        /// A restricted user that only has read only access to certain parts
+        /// of the resource.
+        /// </summary>
+        ReadOnlyUser,
+
+        /// <summary>
+        /// A non-registered user that only have read access to the application.
         /// </summary>
         Guest,
     }
