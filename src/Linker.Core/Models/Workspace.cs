02872d3d08e98d543434caff52be09662436c445
data-miner00 <data-miner00@users.noreply.github.com>
2024-02-01 23:21:23 +0800

    feat: Add workspace model

---
+++ src/Linker.Core/Models/Workspace.cs
@@ -0,0 +1,43 @@
+﻿#nullable disable
+namespace Linker.Core.Models;
+
+/// <summary>
+/// Allow users to create meaningful collection and making sense of the links.
+/// </summary>
+public class Workspace
+{
+    /// <summary>
+    /// Gets or sets the unique Id.
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the "username" of the workspace for easy reference.
+    /// </summary>
+    public string Handle { get; set; }
+
+    /// <summary>
+    /// Gets or sets the display name of the workspace.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the workspace.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the owner Id.
+    /// </summary>
+    public string OwnerId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the creation date.
+    /// </summary>
+    public string CreatedAt { get; set; }
+
+    /// <summary>
+    /// Gets or sets the last modified date.
+    /// </summary>
+    public string ModifiedAt { get; set; }
+}
