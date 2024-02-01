3a6be4803a15cc1aba276f91e5c9ba87587cdbf1
data-miner00 <data-miner00@users.noreply.github.com>
2024-02-01 20:53:50 +0800

    docs: Add missing docstrings

--- src/Linker.WebApi/Filters/RoleAuthorizeAttribute.cs
+++ src/Linker.WebApi/Filters/RoleAuthorizeAttribute.cs
@@ -5,11 +5,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Security.Claims;
 
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+/// <summary>
+/// Authorize individuals with the exact role required.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
 public sealed class RoleAuthorizeAttribute : Attribute, IAuthorizationFilter
 {
     private readonly Role role;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RoleAuthorizeAttribute"/> class.
+    /// </summary>
+    /// <param name="role">The exact role required.</param>
     public RoleAuthorizeAttribute(Role role)
     {
         this.role = role;
