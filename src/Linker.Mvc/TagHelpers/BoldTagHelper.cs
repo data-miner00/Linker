70cbee85d845f7bc948408abff2acc552730e4f4
data-miner00 <data-miner00@users.noreply.github.com>
2024-02-27 09:06:41 +0800

    feat: Use tag helpers

---
+++ src/Linker.Mvc/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Linker.Mvc.TagHelpers;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+/// <summary>
+/// Tag helper to create a bolded text within an element or an element itself.
+/// </summary>
+[HtmlTargetElement("bold")]
+[HtmlTargetElement(Attributes = "bold")]
+public class BoldTagHelper : TagHelper
+{
+    /// <inheritdoc/>
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.RemoveAll("bold");
+        output.PreContent.SetHtmlContent("<strong class=\"font-bold\">");
+        output.PostContent.SetHtmlContent("</strong>");
+    }
+}
---
+++ src/Linker.Mvc/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,37 @@
+﻿namespace Linker.Mvc.TagHelpers;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+/// <summary>
+/// Tag helper to create an email anchor.
+/// </summary>
+[HtmlTargetElement("email", TagStructure = TagStructure.NormalOrSelfClosing)]
+public class EmailTagHelper : TagHelper
+{
+    private const string EmailDomain = "gmail.com";
+
+    /// <summary>
+    /// Gets or sets the email recipient.
+    /// </summary>
+    [HtmlAttributeName("recipient")]
+    required public string MailTo { get; set; }
+
+    /// <summary>
+    /// Gets the full email address.
+    /// </summary>
+    public string EmailAddress => $"{this.MailTo}@{EmailDomain}";
+
+    /// <inheritdoc/>
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        ConvertToAnchorLink(output);
+
+        output.Attributes.SetAttribute("href", "mailto:" + this.EmailAddress);
+        output.Content.SetContent(this.EmailAddress);
+    }
+
+    private static void ConvertToAnchorLink(TagHelperOutput output)
+    {
+        output.TagName = "a";
+    }
+}
