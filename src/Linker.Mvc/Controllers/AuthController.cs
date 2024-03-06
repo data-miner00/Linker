f3c328f3c27c963285f4671c5def6d259550dd6b
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-06 21:24:36 +0800

    feat: Auth pages

---
+++ src/Linker.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,131 @@
+﻿namespace Linker.Mvc.Controllers;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Linker.Core.ApiModels;
+using Linker.Core.Repositories;
+using AutoMapper;
+using Linker.Core.Models;
+
+public sealed class AuthController : Controller
+{
+    private readonly IUserRepository repository;
+    private readonly IMapper mapper;
+
+    public AuthController(IUserRepository repository, IMapper mapper)
+    {
+        ArgumentNullException.ThrowIfNull(repository);
+        ArgumentNullException.ThrowIfNull(mapper);
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    // GET: AuthController/Login
+    public IActionResult Login()
+    {
+        if (this.HttpContext.User is not null &&
+            this.HttpContext.User.Identity is not null &&
+            this.HttpContext.User.Identity.IsAuthenticated)
+        {
+            return this.RedirectToAction("Index", "Home");
+        }
+
+        return this.View();
+    }
+
+    // POST: AuthController/Login
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginRequest request)
+    {
+        try
+        {
+            var user = await this.repository
+                .GetByUsernameAndPasswordAsync(request.Username, request.Password, default)
+                .ConfigureAwait(false);
+
+            Claim[] claims = [
+                new(ClaimTypes.NameIdentifier, user.Id),
+                new(ClaimTypes.Name, user.Username),
+                new(ClaimTypes.Role, user.Role.ToString()),
+                new(ClaimTypes.DateOfBirth, user.DateOfBirth.ToShortDateString()),
+            ];
+            ClaimsIdentity claimsIdentity = new(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            ClaimsPrincipal principal = new(claimsIdentity);
+
+            AuthenticationProperties properties = new()
+            {
+                AllowRefresh = true,
+                IsPersistent = true,
+            };
+
+            await this.HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                principal,
+                properties);
+
+            return this.RedirectToAction("Index", "Home");
+        }
+        catch (InvalidOperationException)
+        {
+            this.TempData["error"] = "Username or password wrong.";
+
+            return this.View(request);
+        }
+        catch
+        {
+            this.TempData["error"] = "Something wrong";
+
+            return this.View(request);
+        }
+    }
+
+    // GET: AuthController/Register
+    public IActionResult Register()
+    {
+        if (this.HttpContext.User is not null &&
+            this.HttpContext.User.Identity is not null &&
+            this.HttpContext.User.Identity.IsAuthenticated)
+        {
+            return this.RedirectToAction("Index", "Home");
+        }
+
+        return this.View();
+    }
+
+    // POST: AuthController/Register
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(CreateUserRequest request)
+    {
+        var user = this.mapper.Map<User>(request);
+
+        try
+        {
+            await this.repository
+                .AddAsync(user, default)
+                .ConfigureAwait(false);
+
+            this.TempData["success"] = "Successfully registered.";
+
+            return this.RedirectToAction(nameof(this.Login));
+        }
+        catch (InvalidOperationException)
+        {
+            this.TempData["error"] = "User already exist.";
+            return this.View(request);
+        }
+    }
+
+    // GET: AuthController/Logout
+    public async Task<IActionResult> Logout()
+    {
+        await this.HttpContext
+            .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        return this.RedirectToAction(nameof(this.Login));
+    }
+}
--- src/Linker.Mvc/Program.cs
+++ src/Linker.Mvc/Program.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Linker.Common.Mappers;
 using Linker.Core.Repositories;
 using Linker.Data.SQLite;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,11 +13,19 @@ var sqliteConnectionString = builder.Configuration["SQLiteOption:ConnectionStrin
 using var connection = new SQLiteConnection(sqliteConnectionString);
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = "/Auth/Login";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+    });
+
 builder.Services
     .AddSingleton<IDbConnection>(connection)
     .AddSingleton<IArticleRepository, ArticleRepository>()
     .AddSingleton<IWebsiteRepository, WebsiteRepository>()
-    .AddSingleton<IYoutubeRepository, YoutubeRepository>();
+    .AddSingleton<IYoutubeRepository, YoutubeRepository>()
+    .AddSingleton<IUserRepository, UserRepository>();
 
 builder.Services
     .AddSingleton(new MapperConfiguration(config =>
@@ -25,7 +34,10 @@ builder.Services
         config.AddProfile<ArticleMapperProfile>();
         config.AddProfile<WebsiteMapperProfile>();
         config.AddProfile<YoutubeMapperProfile>();
+        config.AddProfile<UserMapperProfile>();
     }).CreateMapper());
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
@@ -42,10 +54,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Auth}/{action=Login}/{id?}");
 
 app.Run();
