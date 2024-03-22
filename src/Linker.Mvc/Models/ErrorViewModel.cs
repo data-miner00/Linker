9edeea02f5282cb55a1b68f0aed11717c2f06b04
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-23 06:17:14 +0800

    doc: NotNullIfNotNull

--- src/Linker.Mvc/Models/ErrorViewModel.cs
+++ src/Linker.Mvc/Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
 namespace Linker.Mvc.Models;
 
+using System.Diagnostics.CodeAnalysis;
+
 public class ErrorViewModel
 {
+    [NotNullIfNotNull(nameof(RequestId))]
     public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
 }
