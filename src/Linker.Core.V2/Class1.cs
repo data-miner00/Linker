0691c20e340c10b6424155bc5eaee1b6be4cc902
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-26 22:16:29 +0800

    feat: Add asset uploader interface

--- src/Linker.Core.V2/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Linker.Core.V2;
-
-public class Class1
-{
-
-}
---
+++ src/Linker.Core.V2/IAssetUploader.cs
@@ -0,0 +1,19 @@
+﻿namespace Linker.Core.V2;
+
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Specifies an asset uploader.
+/// </summary>
+public interface IAssetUploader
+{
+    /// <summary>
+    /// Uploads a file to the server.
+    /// </summary>
+    /// <param name="id">The Id of the file.</param>
+    /// <param name="file">The file object.</param>
+    /// <returns>The task.</returns>
+    Task UploadAsync(Guid id, IFormFile file);
+}
