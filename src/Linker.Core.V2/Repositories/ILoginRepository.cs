d9c52ce6ba665545038823f9775119b26ef2eb40
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-29 23:07:18 +0800

    feat: Add login repository

---
+++ src/Linker.Core.V2/Repositories/ILoginRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace Linker.Core.V2.Repositories;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The repository to track login dates.
+/// </summary>
+public interface ILoginRepository
+{
+    /// <summary>
+    /// Tracks the login for the user.
+    /// </summary>
+    /// <param name="userId">The user ID.</param>
+    /// <returns>The task.</returns>
+    Task TrackLoginAsync(string userId);
+
+    /// <summary>
+    /// Retrieves the last login date tracked.
+    /// </summary>
+    /// <param name="userId">The user ID.</param>
+    /// <returns>The login date.</returns>
+    Task<DateTime> GetLastLoginDateAsync(string userId);
+
+    /// <summary>
+    /// Retrieves a list of recent logins performed by the user.
+    /// </summary>
+    /// <param name="userId">The user ID.</param>
+    /// <param name="limit">The number of records to retrieve.</param>
+    /// <returns>The list of login dates.</returns>
+    Task<IEnumerable<DateTime>> GetRecentLoginDatesAsync(string userId, int limit);
+}
