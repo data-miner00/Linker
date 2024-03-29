24165f8f232d2f1ade72455970ec9bbd7837c637
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-30 00:04:41 +0800

    chore: Debug sql server

---
+++ src/Linker.Data/SqlServer/LinkRepository.cs
@@ -0,0 +1,74 @@
+﻿namespace Linker.Data.SqlServer;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Linker.Core.V2.Models;
+using Linker.Core.V2.Repositories;
+using Dapper;
+using System.Data;
+using EnsureThat;
+
+/// <summary>
+/// The repository for working with <see cref="Link"/>.
+/// </summary>
+public sealed class LinkRepository : ILinkRepository
+{
+    private readonly IDbConnection connection;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LinkRepository"/> class.
+    /// </summary>
+    /// <param name="connection">The database connection.</param>
+    public LinkRepository(IDbConnection connection)
+    {
+        this.connection = EnsureArg.IsNotNull(connection, nameof(connection));
+    }
+
+    /// <inheritdoc/>
+    public Task AddAsync(Link link, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<IEnumerable<Link>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var query = @"SELECT * FROM Links;";
+
+        return this.connection.QueryAsync<Link>(query, cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public Task<IEnumerable<Link>> GetAllByUserAsync(string userId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<Link> GetByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<Link> GetByUserAsync(string userId, string linkId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task RemoveAsync(string id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task UpdateAsync(Link item, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Linker.Debugger/Program.cs
+++ src/Linker.Debugger/Program.cs
@@ -3,84 +3,63 @@
     using System;
     using System.Configuration;
     using System.Data;
+    using System.Data.SqlClient;
     using System.Data.SQLite;
     using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
     using System.Timers;
     using Dapper;
-    using Linker.Core.Models;
+    using Linker.Core.V2.Models;
     using Linker.Data.SQLite;
+    using Linker.Data.SqlServer;
 
     internal static class Program
     {
         public static void Main(string[] args)
         {
-            using (var connection = new SQLiteConnection(LoadConnectionString()))
+            var connectionString = LoadConnectionString("SqlServer");
+            using var connection = new SqlConnection(connectionString);
+
+            var a = GetAll(connection).GetAwaiter().GetResult();
+            Console.WriteLine();
+        }
+
+        public static Task<IEnumerable<Link>> GetAll(IDbConnection connection)
+        {
+            var linkRepo = new LinkRepository(connection);
+            return linkRepo.GetAllAsync(default);
+        }
+
+        public static Task AddLink(IDbConnection connection)
+        {
+            var linkRepo = new LinkRepository(connection);
+
+            var link = new Link
             {
-                var tagRepo = new TagRepository(connection);
-                goto some;
-            some:
-                var tags = tagRepo.GetAllAsync().GetAwaiter().GetResult();
+                Id = Guid.NewGuid().ToString(),
+                Url = "sample.com/article.htm",
+                Category = Category.Finance,
+                Description = "long desc",
+                Tags = new List<string> { "tag1", "tag2", "new TAG!!!" },
+                Language = Language.Japanese,
+                Type = LinkType.Website,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now,
+                Name = "Crummy title",
+                KeyPersonName = "Jim Cramer",
+                Visibility = Visibility.Private,
+                Domain = "www.google.com",
+                Grammar = Grammar.Average,
+                Rating = Rating.PG13,
+            };
 
-                Console.WriteLine(tags);
-            }
-        }
-
-        public static async Task AddArticle(IDbConnection connection)
-        {
-            var articleRepo = new ArticleRepository(connection);
-
-            await articleRepo.AddAsync(
-                new Article
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Url = "sample.com/article.htm",
-                    Category = Category.Finance,
-                    Description = "long desc",
-                    Tags = new List<string> { "tag1", "tag2", "new TAG!!!" },
-                    Language = Language.Japanese,
-                    LastVisitAt = DateTime.Now,
-                    CreatedAt = DateTime.Now,
-                    ModifiedAt = DateTime.Now,
-                    Title = "Crummy title",
-                    Author = "Jim Cramer",
-                    Year = 2014,
-                    WatchLater = true,
-                    Domain = "www.google.com",
-                    Grammar = Grammar.Average,
-                },
-                CancellationToken.None);
-
-            await articleRepo.GetAllAsync(CancellationToken.None);
-        }
-
-        public static async Task AddYoutube(IDbConnection connection)
-        {
-            var youtubeRepo = new YoutubeRepository(connection);
-            await youtubeRepo.UpdateAsync(
-                new Youtube
-                {
-                    Id = "ae58d402-b7a4-4e69-97d6-30ebcad741c5",
-                    Url = "youtube.com/dahuhffd",
-                    Category = Category.Finance,
-                    Description = "A C# professional guru",
-                    Tags = new List<string> { "c#", "unity", "f#" },
-                    Language = Language.English,
-                    LastVisitAt = new DateTime(1994, 4, 5, 0, 0, 0, DateTimeKind.Utc),
-                    CreatedAt = new DateTime(2022, 4, 5, 0, 0, 0, DateTimeKind.Utc),
-                    ModifiedAt = DateTime.Now,
-                    Name = "Brackeys",
-                    Youtuber = "Johnson",
-                    Country = "Canada",
-                },
-                CancellationToken.None);
-
-            var all = await youtubeRepo.GetAllAsync(CancellationToken.None);
+            return linkRepo.AddAsync(link, default);
         }
 
         private static string LoadConnectionString(string id = "Default")
         {
-            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+            goto next;
+            next: return ConfigurationManager.ConnectionStrings[id].ConnectionString;
         }
     }
 }
