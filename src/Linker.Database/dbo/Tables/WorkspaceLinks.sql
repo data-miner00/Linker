a1f4dc985b48a279f859e2b67e4638e25c76ea6a
data-miner00 <data-miner00@users.noreply.github.com>
2024-04-01 18:05:39 +0800

    fix: Add missing workspace links schema

---
+++ src/Linker.Database/dbo/Tables/WorkspaceLinks.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[WorkspaceLinks] (
+    [WorkspaceId] NVARCHAR (50) NOT NULL,
+    [LinkId]      NVARCHAR (50) NOT NULL,
+    [CreatedAt]   DATETIME2 (7) NOT NULL,
+    CONSTRAINT [PK_WorkspaceLinks] PRIMARY KEY CLUSTERED ([WorkspaceId] ASC, [LinkId] ASC),
+    CONSTRAINT [FK_WorkspaceLinks_Links] FOREIGN KEY ([LinkId]) REFERENCES [dbo].[Links] ([Id]),
+    CONSTRAINT [FK_WorkspaceLinks_Workspaces] FOREIGN KEY ([WorkspaceId]) REFERENCES [dbo].[Workspaces] ([Id])
+);
+
+
+
