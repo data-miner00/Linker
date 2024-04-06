11d5475087aac657975c7283824f39ddfbc65243
data-miner00 <data-miner00@users.noreply.github.com>
2024-04-06 18:45:40 +0800

    feat: Add chat message model

---
+++ src/Linker.Core.V2/Models/ChatMessage.cs
@@ -0,0 +1,49 @@
+﻿namespace Linker.Core.V2.Models;
+
+using System;
+
+/// <summary>
+/// The chat object.
+/// </summary>
+public sealed record ChatMessage
+{
+    /// <summary>
+    /// Gets or sets the Id of the chat record.
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the author's ID.
+    /// </summary>
+    public string AuthorId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the workspace's ID.
+    /// </summary>
+    public string WorkspaceId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the actual chat message.
+    /// </summary>
+    public string Content { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the chat content has been edited.
+    /// </summary>
+    public bool IsEdited { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the chat content has been deleted.
+    /// </summary>
+    public bool IsDeleted { get; set; }
+
+    /// <summary>
+    /// Gets or sets the created date.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+
+    /// <summary>
+    /// Gets or set the updated date.
+    /// </summary>
+    public DateTime ModifiedAt { get; set; }
+}
