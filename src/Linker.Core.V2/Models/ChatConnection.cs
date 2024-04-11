730a9872294ed6d81845f98eebcda8e2e967d1d2
data-miner00 <data-miner00@users.noreply.github.com>
2024-04-11 22:36:56 +0800

    feat: Working basic chat connection

---
+++ src/Linker.Core.V2/Models/ChatConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace Linker.Core.V2.Models;
+
+public class ChatConnection
+{
+    public string Username { get; set; }
+
+    public string RoomId { get; set; }
+}
--- src/Linker.Mvc/Hubs/ChatHub.cs
+++ src/Linker.Mvc/Hubs/ChatHub.cs
@@ -1,11 +1,25 @@
 ﻿namespace Linker.Mvc.Hubs;
 
+using Linker.Core.V2.Models;
 using Microsoft.AspNetCore.SignalR;
 
 public class ChatHub : Hub
 {
+    private const string ServerIdentifier = "server";
+
     public Task SendMessage(string userId, string message)
     {
         return this.Clients.All.SendAsync("ReceiveMessage", userId, message);
+    }
+
+    public Task JoinChat(ChatConnection chat)
+    {
+        return this.Clients.All.SendAsync("ReceiveMessage", ServerIdentifier, $"{chat.Username} has joined");
+    }
+
+    public async Task JoinSpecificChatRoom(ChatConnection chat)
+    {
+        await this.Groups.AddToGroupAsync(this.Context.ConnectionId, chat.RoomId);
+        await this.Clients.Group(chat.RoomId).SendAsync("JoinSpecificChatRoom", ServerIdentifier, $"{chat.Username} has joined {chat.RoomId}!");
     }
 }
