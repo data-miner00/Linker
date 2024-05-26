4588dcfbc384b4cda6d8c8ab8bb61db8259c7559
data-miner00 <data-miner00@users.noreply.github.com>
2024-05-26 20:41:11 +0800

    feat: Add task extension util

---
+++ src/Linker.Common/Extensions/TaskExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Linker.Common.Extensions;
+
+using System;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Extension class for <see cref="Task"/> related code.
+/// </summary>
+public static class TaskExtensions
+{
+    /// <summary>
+    /// Safely execute the <see cref="Task"/> without waiting for it to complete before moving to the next instruction.
+    /// </summary>
+    /// <param name="task">The task itself.</param>
+    /// <param name="continueOnCapturedContext">Whether continue on the calling thread.</param>
+    /// <param name="onException">Action to execute during exception.</param>
+#pragma warning disable S3168 // "async" methods should not return "void"
+    public static async void SafeFireAndForget(
+        this Task task,
+        bool continueOnCapturedContext = true,
+        Action<Exception> onException = null)
+    {
+        try
+        {
+            await task.ConfigureAwait(continueOnCapturedContext);
+        }
+        catch (Exception ex) when (onException is not null)
+        {
+            onException(ex);
+        }
+    }
+#pragma warning restore S3168 // "async" methods should not return "void"
+}
