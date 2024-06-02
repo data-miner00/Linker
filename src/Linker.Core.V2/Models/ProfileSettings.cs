b66bba97bfca9cad9b0598c3891bdb22b014d8f6
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-02 08:57:05 +0800

    feat: Add profile setting model

---
+++ src/Linker.Core.V2/Models/ProfileSettings.cs
@@ -0,0 +1,44 @@
+﻿namespace Linker.Core.V2.Models;
+
+using System;
+
+/// <summary>
+/// The settings for a user.
+/// </summary>
+public class ProfileSettings
+{
+    /// <summary>
+    /// Gets or sets the Id of the setting.
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the theme preferred by the user.
+    /// </summary>
+    public string Theme { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the user has accepted the telemetry.
+    /// </summary>
+    public bool AcceptedTelemetry { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the user has accepted the terms and conditions.
+    /// </summary>
+    public bool AcceptedTerms { get; set; }
+
+    /// <summary>
+    /// Gets or sets the browsing level preferred by the user.
+    /// </summary>
+    public Rating BrowsingLevel { get; set; }
+
+    /// <summary>
+    /// Gets or sets the creation date.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+
+    /// <summary>
+    /// Gets or sets the last updated date.
+    /// </summary>
+    public DateTime ModifiedAt { get; set; }
+}
