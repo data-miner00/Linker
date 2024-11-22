a0e6d33c7353cbbeb4104c77aa943daf3b7d574c
Shaun Chong <data-miner00@users.noreply.github.com>
2024-11-22 21:52:47 +0800

    test: Empty fixture

---
+++ src/Linker.WebApi.IntegrationTests/ContainerConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Linker.WebApi.IntegrationTests;
+
+using Autofac;
+
+internal static class ContainerConfig
+{
+    public static IContainer Configure()
+    {
+        var builder = new ContainerBuilder();
+
+        return builder.Build();
+    }
+}
---
+++ src/Linker.WebApi.IntegrationTests/TestFixture.cs
@@ -0,0 +1,13 @@
+﻿namespace Linker.WebApi.IntegrationTests;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+public sealed class TestFixture : IClassFixture<TestFixture>
+{
+    
+}
