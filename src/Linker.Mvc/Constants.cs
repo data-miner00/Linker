5c9e8a90b513133985f88f5e215121a6dc01b0f4
Shaun Chong <data-miner00@users.noreply.github.com>
2024-11-22 22:03:49 +0800

    chore: Stub image first

--- src/Linker.Mvc/Constants.cs
+++ src/Linker.Mvc/Constants.cs
@@ -14,4 +14,9 @@ public static class Constants
     /// The error key for <c>TempData</c>.
     /// </summary>
     public const string Error = "error";
+
+    /// <summary>
+    /// The random ads image. Will remove soon.
+    /// </summary>
+    public const string RandomStubAdsImageUrl = "https://i.pinimg.com/736x/33/22/23/33222302e137bc0a21a4b195c5a53c09.jpg";
 }
