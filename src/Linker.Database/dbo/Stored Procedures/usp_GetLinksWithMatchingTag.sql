b6eee2115867552f6e712f577abd972fb565ccd7
Shaun Chong <data-miner00@users.noreply.github.com>
2024-12-08 20:44:52 +0800

    feat: Add get links with matching tags procedure

---
+++ src/Linker.Database/dbo/Stored Procedures/usp_GetLinksWithMatchingTag.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_GetLinksWithMatchingTag]
+	@Tag NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT * FROM [dbo].[vw_TaggedLinks]
+	WHERE [dbo].[vw_TaggedLinks].[Tags] LIKE '%' + @Tag + '%'
+END
---
+++ src/Linker.Database/dbo/Views/vw_TaggedLinks.sql
@@ -0,0 +1,54 @@
+﻿CREATE VIEW [dbo].[vw_TaggedLinks]
+WITH SCHEMABINDING
+AS
+SELECT l.[Id]
+      ,l.[Name]
+      ,[Domain]
+      ,[Url]
+      ,[Description]
+      ,[AddedBy]
+      ,[IsSubdomain]
+      ,[IsMultilingual]
+      ,[IsResource]
+      ,[Category]
+      ,[Language]
+      ,[Rating]
+      ,[Aesthetics]
+      ,[Type]
+      ,[Country]
+      ,[KeyPersonName]
+      ,[Grammar]
+      ,[Visibility]
+      ,l.[CreatedAt]
+      ,l.[ModifiedAt]
+      ,[ThumbnailUrl]
+      ,[FaviconUrl]
+	  ,STRING_AGG(t.[Name], ',') AS Tags
+  FROM [dbo].[Links] l
+  JOIN [dbo].[LinksTags] lt
+  ON lt.LinkId = l.Id
+  JOIN [dbo].[Tags] t
+  ON lt.TagId = t.Id
+  GROUP BY
+	  l.[Id]
+      ,l.[Name]
+      ,[Domain]
+      ,[Url]
+      ,[Description]
+      ,[AddedBy]
+      ,[IsSubdomain]
+      ,[IsMultilingual]
+      ,[IsResource]
+      ,[Category]
+      ,[Language]
+      ,[Rating]
+      ,[Aesthetics]
+      ,[Type]
+      ,[Country]
+      ,[KeyPersonName]
+      ,[Grammar]
+      ,[Visibility]
+      ,l.[CreatedAt]
+      ,l.[ModifiedAt]
+      ,[ThumbnailUrl]
+      ,[FaviconUrl];
