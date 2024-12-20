e9ea3945887b577b8a6f0ee6226ae3a81761ae1c
Shaun Chong <data-miner00@users.noreply.github.com>
2024-12-20 22:38:54 +0800

    feat: Add square tag helper

---
+++ src/Linker.Mvc/TagHelpers/SquareTagHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Linker.Mvc.TagHelpers;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+[HtmlTargetElement("square", TagStructure = TagStructure.WithoutEndTag)]
+public sealed class SquareTagHelper : TagHelper
+{
+    public string ColorClass { get; set; } = "bg-gray-100";
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        output.Attributes.Add("class", this.ColorClass + " w-4 h-4");
+    }
+}
