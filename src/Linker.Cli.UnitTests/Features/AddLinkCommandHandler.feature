96bec36bbf3140551a874ac2684f6625b451f31d
Shaun Chong <data-miner00@users.noreply.github.com>
2025-04-25 23:26:34 +0800

    test: Add tests for AddLinkCommandHandler

---
+++ src/Linker.Cli.UnitTests/Features/AddLinkCommandHandler.feature
@@ -0,0 +1,53 @@
+﻿Feature: AddLinkCommandHandler
+
+The behaviour for the AddLinkCommandHandler.
+
+@constructor
+Scenario: When the repository is null, it should throw an exception
+	Given the repository is null
+	When I instantiate the AddLinkCommandHandler
+	Then I should expect an exception to be thrown
+
+@handle @arguments
+Scenario: Handling a null command argument, it should throw an exception
+    Given the command argument is null
+	When I handle the command arguments
+	Then I should expect an exception to be thrown
+
+@handle @arguments
+Scenario: Handling an invalid type argument, it should throw an exception
+    Given the command argument is incorrect type
+	When I handle the command arguments
+	Then I should expect ArgumentException to be thrown
+
+@handle @arguments
+Scenario: Given request for help, should do nothing
+    Given the command argument is help
+	When I handle the command arguments
+	Then I should expect AddAsync to not be called
+    And I should expect no exception is thrown
+
+@handle @arguments
+Scenario: Given a valid command argument, it should call the repository
+    Given the command with url "https://google.com"
+	When I handle the command arguments
+	Then I should expect AddAsync to be called with command with url "https://google.com"
+    And I should expect no exception is thrown
+
+@handle @repository
+Scenario: Does not allow duplicate URLs
+    Given the url already exists
+    And the command with url "https://google.com"
+	When I handle the command arguments
+	Then I should expect AddAsync to be called with command with url "https://google.com"
+	And I should expect environment exit code to be 1
+    And I should expect no exception is thrown
+
+@handle @repository
+Scenario: Uncaught exception occurred
+    Given the command with url "https://google.com"
+	And SqliteException occurs
+	When I handle the command arguments
+	Then I should expect AddAsync to be called with command with url "https://google.com"
+	And I should expect SqliteException to be thrown
+
---
+++ src/Linker.Cli.UnitTests/StepDefinitions/AddLinkCommandHandlerStepDefinitions.cs
@@ -0,0 +1,149 @@
+﻿namespace Linker.Cli.UnitTests.StepDefinitions;
+
+using Linker.Cli.Handlers;
+using Linker.Cli.Integrations;
+using Linker.Cli.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linker.TestCore;
+using Moq;
+using Linker.Cli.Commands;
+using Xunit.Sdk;
+using Microsoft.Data.Sqlite;
+
+[Binding]
+internal class AddLinkCommandHandlerStepDefinitions : BaseSteps<AddLinkCommandHandlerStepDefinitions>
+{
+    private IRepository<Link>? repository;
+    private Mock<IRepository<Link>> mockRepository;
+    private AddLinkCommandHandler commandHandler;
+    private object? commandArguments;
+
+    public AddLinkCommandHandlerStepDefinitions()
+    {
+        this.mockRepository = new Mock<IRepository<Link>>();
+        this.commandHandler = new AddLinkCommandHandler(this.mockRepository.Object);
+    }
+
+    [Given("the repository is null")]
+    public void GivenTheRepositoryIsNull()
+    {
+        this.repository = null;
+    }
+
+    [Given("the command argument is null")]
+    public void GivenTheCommandArgumentIsNull()
+    {
+        this.commandArguments = null;
+    }
+
+    [Given("the command argument is incorrect type")]
+    public void GivenTheCommandArgumentIsIncorrectType()
+    {
+        this.commandArguments = new object();
+    }
+
+    [Given("the command argument is help")]
+    public void GivenTheCommandArgumentIsHelp()
+    {
+        this.commandArguments = new AddLinkCommandArguments
+        {
+            ShowHelp = true,
+        };
+    }
+
+    [Given(@"the command with url ""(.*?)""")]
+    public void GivenTheCommandWithUrl(string url)
+    {
+        this.commandArguments = new AddLinkCommandArguments
+        {
+            Url = url,
+        };
+    }
+
+    [Given(@"the url already exists")]
+    public void GivenUrlAlreadyExists()
+    {
+        var exception = new Exception(
+            "Outer error",
+            new Exception("SQLite Error 19: 'UNIQUE constraint failed: Links.Url'"));
+
+        this.mockRepository
+            .Setup(x => x.AddAsync(It.IsAny<Link>()))
+            .ThrowsAsync(exception);
+    }
+
+    [Given("SqliteException occurs")]
+    public void GivenSqlExceptionOccurs()
+    {
+        var exception = new SqliteException("Error!", 888);
+
+        this.mockRepository
+            .Setup(x => x.AddAsync(It.IsAny<Link>()))
+            .ThrowsAsync(exception);
+    }
+
+    [When("I instantiate the AddLinkCommandHandler")]
+    public void WhenIInstantiateTheAddLinkCommandHandler()
+    {
+        this.RecordException(() => new AddLinkCommandHandler(this.repository));
+    }
+
+    [When("I handle the command arguments")]
+    public Task WhenIHandleTheCommandArguments()
+    {
+        return this.RecordExceptionAsync(() => this.commandHandler.HandleAsync(this.commandArguments));
+    }
+
+    [Then("I should expect an exception to be thrown")]
+    public void ThenIShouldExpectAnExceptionToBeThrown()
+    {
+        this.ThenIExpectExceptionIsThrown<ArgumentNullException>();
+    }
+
+    [Then("I should expect ArgumentException to be thrown")]
+    public void ThenIShouldExpectArgumentExceptionToBeThrown()
+    {
+        this.ThenIExpectExceptionIsThrown<ArgumentException>();
+    }
+
+    [Then("I should expect SqliteException to be thrown")]
+    public void ThenIShouldExpectSqliteExceptionToBeThrown()
+    {
+        this.ThenIExpectExceptionIsThrown<SqliteException>();
+    }
+
+    [Then("I should expect no exception is thrown")]
+    public void ThenIShouldExpectNoExceptionIsThrown()
+    {
+        this.ThenIExpectNoExceptionIsThrown();
+    }
+
+    [Then("I should expect AddAsync to not be called")]
+    public void ThenIShouldExpectAddAsyncToNotBeCalled()
+    {
+        this.mockRepository
+            .Verify(x => x.AddAsync(It.IsAny<Link>()), Times.Never());
+    }
+
+    [Then(@"I should expect AddAsync to be called with command with url ""(.*?)""")]
+    public void ThenIShouldExpectAddAsyncToBeCalledWith(string url)
+    {
+        this.mockRepository
+            .Verify(x => x.AddAsync(It.Is<Link>(l => l.Url == url)), Times.Once());
+    }
+
+    [Then("I should expect environment exit code to be (.*)")]
+    public void ThenIShouldExpectEnvironmentExitCodeToBe(int exitCode)
+    {
+        Assert.Equal(exitCode, Environment.ExitCode);
+    }
+
+    public override AddLinkCommandHandlerStepDefinitions GetSteps()
+    {
+        return this;
+    }
+}
