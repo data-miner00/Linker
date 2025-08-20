9720b676ddc3f6804b593be6cbfa9ca28195c878
Shaun Chong <data-miner00@users.noreply.github.com>
2025-08-20 19:16:18 +0800

    feat: Edit profile

---
+++ src/Linker.Core.V2/ApiModels/UpdateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Linker.Core.V2.ApiModels;
+
+using System.ComponentModel.DataAnnotations;
+
+public sealed class UpdateUserRequest
+{
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public string PhotoUrl { get; set; }
+
+    public string Description { get; set; }
+}
